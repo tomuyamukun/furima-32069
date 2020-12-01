e8ea494380e413b3f3f28232f5f08f328334f756
tomuyamukun <3156tomochan@gmail.com>
2020-12-01 12:44:28 +0900

    修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -55,6 +55,8 @@ class ItemsController < ApplicationController
 
   def move_to_login
     @item = Item.find(params[:id])
-    redirect_to user_session_path unless user_signed_in? && current_user.id == @item.user_id
+    unless user_signed_in? && current_user.id == @item.user_id
+     redirect_to user_session_path
+    end
   end
 end
