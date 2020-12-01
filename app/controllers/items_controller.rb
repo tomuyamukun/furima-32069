fbb6434ab7c466d23d5094271b6fb1aaf6c6699b
tomuyamukun <3156tomochan@gmail.com>
2020-12-01 12:16:06 +0900

    修正２回め

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,9 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if user_signed_in? && current_user.id == item.user_id
-      item.destroy
+    @item = Item.find(params[:id])
+    if user_signed_in? && current_user.id == @item.user_id
+      @item.destroy
       redirect_to root_path
     else
       redirect_to user_session_path
