95763d8fced66351a34309c11cd30e39ccb4e2ad
tomuyamukun <3156tomochan@gmail.com>
2020-12-02 10:13:23 +0900

    修正7回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy, :edit]
-  before_action :move_to_index, only: :edit
   before_action :item_find, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -54,7 +54,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
      redirect_to root_path
     end
