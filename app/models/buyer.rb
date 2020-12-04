6cff055af362dbdce035d696d4e57cfd6281caae
tomuyamukun <3156tomochan@gmail.com>
2020-12-04 18:25:48 +0900

    修正1回目

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -2,8 +2,4 @@ class Buyer < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :order
-  with_options presence: true do
-    validates :user
-    validates :item
-  end
 end
--- db/migrate/20201202153830_create_orders.rb
+++ db/migrate/20201202153830_create_orders.rb
@@ -7,7 +7,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.string :house_number,   null: false
       t.string :building_name   
       t.string :phone_number,   null: false
-      # t.references :item, null: false, foreign_key: true
+
       t.references :buyer, null: false, foreign_key: true
       t.timestamps
     end
